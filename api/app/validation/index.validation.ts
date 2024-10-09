b26b2b32ff7c2759a634d455a06257e22d0156dd
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2024-10-09 15:39:24 +0200

    added graphql & apollo dependencies

--- api/app/validation/index.validation.ts
+++
@@ -1,9 +0,0 @@
-import repoCreateSchema from "./schemas/repo.create.schema";
-import langCreateSchema from "./schemas/lang.create.schema";
-import statusCreateSchema from "./schemas/status.create.schema";
-
-export {
-  repoCreateSchema,
-  langCreateSchema,
-  statusCreateSchema
-}
